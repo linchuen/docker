3361c63afc9a9d97af9f62956c798dce19a99769
Aiden <x29587629@gmail.com>
2024-11-24 19:45:17 +0800

    test to clickhouse success

--- cdc/clickhouse/init.sql
+++ cdc/clickhouse/init.sql
@@ -1,11 +1,13 @@
-CREATE TABLE default.`cdc.cdc.order` (
-    `id` UInt64,
+CREATE DATABASE target;
+
+USE target;
+
+CREATE TABLE `order` (
+    `id` Int64,
     `order_id` Int32 COMMENT '訂單ID',
-    `sync` UInt8 DEFAULT 0,
+    `sync` Int16 DEFAULT 0,
     `create_time` DateTime DEFAULT now() COMMENT '創建時間',
     `trans_no` String COMMENT '訂單'
 ) 
 ENGINE = MergeTree()
-PRIMARY KEY `trans_no`
-ORDER BY (`trans_no`)
-SETTINGS index_granularity = 8192;
+ORDER BY (`id`,`order_id`);
--- cdc/mysql/mysql.dockerfile
+++ cdc/mysql/mysql.dockerfile
@@ -1,3 +1,3 @@
-FROM bitnami/mysql:8.0.35
+FROM bitnami/mysql:8.3.0
 
 COPY ./init.sql /docker-entrypoint-initdb.d/
